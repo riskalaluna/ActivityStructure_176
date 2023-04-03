fb663402212c3703358a652ad55ec6ac019f2afb
riskalaluna <riskalaluna@gmail.com>
2023-04-03 14:39:22 +0700

    4

--- ActivityStructure_176/ActivityStructure_176.cpp
+++ ActivityStructure_176/ActivityStructure_176.cpp
@@ -27,5 +27,8 @@ int main()
 	cout << "Masukkan umur: ";
 	cin >> mhs2.umur;
 
-	cout << "\n"
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
 }
