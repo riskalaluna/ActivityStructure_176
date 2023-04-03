bc954ee6f639dad41726289eecb803dc6b37db63
riskalaluna <riskalaluna@gmail.com>
2023-04-03 14:37:30 +0700

    masukkan nim, nama, alamat dan umur

--- ActivityStructure_176/ActivityStructure_176.cpp
+++ ActivityStructure_176/ActivityStructure_176.cpp
@@ -3,5 +3,29 @@
 using namespace std;
 
 struct Mahasiswa {
-
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20210140011";
+	mhs1.nama = "Tasya";
+	mhs1.alamat = "Semarang";
+	mhs1.umur = 20;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\n"
+}
